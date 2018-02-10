7090e188701c9e2429e6f992e9b75e2d63fc3c98
Dean Hooker <deanhooker91@gmail.com>
2018-02-09 20:00:16 -0700

    products display on run

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,34 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    displayProducts();
+});
+
+function displayProducts() {
+    connection.query({
+        sql: 'SELECT * FROM `bamazon`'
+    },
+        function (err, res) {
+            console.log(' ');
+            for (var i = 0; i < res.length; i++) {
+                console.log('');
+                console.log(res[i].item_id);
+                console.log(res[i].product_name);
+                console.log(res[i].price);
+            }
+            console.log(' ');
+            // userPrompt();
+        }
+    );
+}
